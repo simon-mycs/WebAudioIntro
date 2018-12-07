fe6b9126e9409d46d26cf0c443206921c594941a
Simon Camilleri <simon.camilleri@mycs.com>
2018-12-07 17:24:06 +0100

    update

--- script.js
+++ script.js
@@ -1,85 +1,87 @@
 const AudioContext = window.AudioContext || window.webkitAudioContext;
-const audioCtx = new AudioContext();
+const context = new AudioContext();
 
-const masterGain = audioCtx.createGain(); // master gain
-masterGain.connect(audioCtx.destination); // soundcard
-masterGain.gain.value = 0;
+class Sound {
 
-const filter = audioCtx.createBiquadFilter(); // filter node
-filter.connect(masterGain);
-filter.type = 'lowpass';
-filter.frequency.setTargetAtTime(2000, audioCtx.currentTime, 0);
-filter.gain.value = 0.10;
-
-function Oscillator(type, frequency) {
-    const oscillator = audioCtx.createOscillator();
-    const gainNode = audioCtx.createGain();
-
-    gainNode.gain.value = 0.10; // oscillator gain
-    gainNode.connect(filter);
-    oscillator.type = type;
-    oscillator.frequency.value = frequency;
-    oscillator.connect(gainNode);
-
-    return {
-        getType: () => oscillator.type,
-        getFrequency: () => oscillator.frequency.value,
-        setType: (type => { oscillator.type = type }),
-        connect: (destination) => gainNode.connect(destination),
-        disconnect: () => gainNode.disconnect(),
-        getGainNode: () => gainNode,
-        getTuning: () => oscillator.detune.value,
-        setFrequency: (frequency) => { 
-            oscillator.frequency.setValueAtTime( frequency, audioCtx.currentTime )
-        },
-        detune: (value) => oscillator.detune.value = value,
-        start: () => oscillator.start(),
-        stop: () => oscillator.stop(),
+    constructor(context) {
+      this.context = context;
     }
-} 
+  
+    init() {
+      this.oscillator = this.context.createOscillator();
+      this.gainNode = this.context.createGain();
+  
+      this.oscillator.connect(this.gainNode);
+      this.gainNode.connect(this.context.destination);
+      this.oscillator.type = 'sine';
+    }
+  
+    play(value, time) {
+      this.init();
+      this.oscillator.frequency.value = value;
+      this.gainNode.gain.setValueAtTime(1, this.context.currentTime);  
+      this.oscillator.start(time);
+    }
 
-const oscillator1 = Oscillator('sine', 440); // create oscillator1
-const oscillator2 = Oscillator('sine', 440); // create oscillator2
+    getState() {
+      return this.oscillator.context.state;
+    }
+  
+    stop(time) {
+      this.gainNode.gain.exponentialRampToValueAtTime(0.001, time + 1);
+      this.oscillator.stop(time + 1);
+    }
+  
+  }
 
-// event handlers
+  let oscillatorPool = [];
+  
+  document.addEventListener('keydown', (event) => {
+    if ( ['a', 's', 'd', 'f', 'g', 'h', 'j', 'k'].indexOf(event.key) === -1 || event.repeat)  {
+        return;
+    }
+    const note = new Sound(context);
+    oscillatorPool.push(note);
+    note.play(keyToNoteFrequency(event.key), context.currentTime);
 
-function adjustVolume(value) {
-    masterGain.gain.value = value;
-    filter.gain.value = value;
-}
-function playSound() {
-    oscillator1.start();
-    oscillator2.start();
-}
-function stopSound() {
-    oscillator1.stop();
-    oscillator2.stop();
-}
-function updateOscillatorOneType(waveType) {
-    oscillator1.setType(waveType);
-}
-function updateOscillatorTwoType(waveType) {
-    oscillator2.setType(waveType);
-}
-function detuneOscillator(cents, oscillator) {
-    (eval(oscillator).detune(cents));
-}
-function mute(checkbox) {
-    checkbox.checked ? eval(checkbox.id).connect(filter) : eval(checkbox.id).disconnect();
-}
-function filterCutoff(frequencyHz) {
-    filter.frequency.setTargetAtTime(frequencyHz, audioCtx.currentTime, 0);
-}
-function transposeOctaveDown(parent) {
-    (parent === 'octaveDown1') 
-      ? oscillator1.setFrequency(oscillator1.getFrequency() / 2)
-      : oscillator2.setFrequency(oscillator2.getFrequency() / 2)
-}
-function transposeOctaveUp(parent) {
-    (parent === 'octaveUp1') 
-      ? oscillator1.setFrequency(oscillator1.getFrequency() * 2)
-      : oscillator2.setFrequency(oscillator2.getFrequency() * 2)
-}
-function transpose(frequency) {
-    oscillator1.setFrequency(frequency);
-}
+  }, false);
+
+  document.addEventListener('keyup', (event) => {
+    oscillatorPool.forEach(note => {
+        if (Math.floor(note.oscillator.frequency.value) === Math.floor(keyToNoteFrequency(event.key))) {
+            note.stop(context.currentTime);
+        }
+    });
+  }, false);
+
+  function keyToNoteFrequency(keyPressed) {
+      switch (keyPressed) {
+        case 'a':
+            return 261.6300048828125;
+            break;
+        case 's':
+            return 293.6600036621094;
+            break;    
+        case 'd':
+            return 329.6300048828125;
+            break;    
+        case 'f':
+            return 349.2300109863281;
+            break;    
+        case 'g':
+            return 392;
+            break;    
+        case 'h':
+            return 440;
+            break;    
+        case 'j':
+            return 493.8800048828125;
+            break;    
+        case 'k':
+            return 523.25;
+            break;    
+        default:
+            return 0;
+      }
+  }
+
